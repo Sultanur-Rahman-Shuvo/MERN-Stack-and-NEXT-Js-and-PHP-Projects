da478f83773b1d707bdc18f4e64998001301eaf7
Sultanur-Rahman-Shuvo <138382351+Sultanur-Rahman-Shuvo@users.noreply.github.com>
2023-11-02 18:56:30 +0600

    added a new project

---
+++ Bulk_Rename/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const replaceThis = "shuvo"
+const replaceWith = "Shuvo"
+const preview = false
+const folder = __dirname
+try {
+  const data = fs.readdir(folder, (err, data)=>{
+    for (let index = 0; index < data.length; index++) {
+        const item = data[index];
+        let oldFile = path.join(folder , item)
+        let newFile = path.join(folder , item.replaceAll(replaceThis,replaceWith))
+        if (!preview) {
+            
+            fs.rename(oldFile, newFile, ()=>{
+                console.log("Rename Success")
+            })
+        }else{
+            if ("data/" + item !== newFile) {
+                console.log("data/" + item + " will be renamed to " + newFile)
+            }
+        }
+    }
+  });
+  console.log(data);
+} catch (err) {
+  console.error(err);
+}
