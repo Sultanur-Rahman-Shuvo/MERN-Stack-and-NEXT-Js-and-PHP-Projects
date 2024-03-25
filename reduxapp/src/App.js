8323964cd660bb500c3a2343dd01d9dcfd5884e6
Sultanur-Rahman-Shuvo <138382351+Sultanur-Rahman-Shuvo@users.noreply.github.com>
2024-03-25 22:38:02 +0600

    new react project

---
+++ reduxapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Navbar';
+import Shop from './Shop';
+
+function App() {
+  return (
+    <><Navbar></Navbar>
+      <div className="container">
+        <Shop></Shop>
+      </div></>
+
+  );
+}
+
+export default App;
